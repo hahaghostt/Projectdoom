d5466a91d3dd456166a464f2e84b00fa1c5d7fab
Radeah <113103602+Radeah@users.noreply.github.com>
2023-03-27 21:10:10 +0100

    text code

---
+++ Assets/Scrips/ShowUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+    public GameObject uiObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider Player)
+    {
+        if(Player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
