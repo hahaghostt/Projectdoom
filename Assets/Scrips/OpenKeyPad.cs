5b7f665b4fe839c412c341455bbb34efd3008c42
Radeah <113103602+Radeah@users.noreply.github.com>
2023-04-19 00:41:37 +0100

    fix door keypad yes

--- Assets/Scrips/OpenKeyPad.cs
+++ Assets/Scrips/OpenKeyPad.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class OpenKeyPad : MonoBehaviour
 {
-    public GameObject keypad;
+    public GameObject keyPadObjv;
+    public GameObject keyPadText;
+
+    public bool inReach;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            keyPadText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            keyPadText.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Interact") && inReach)
+        {
+            keyPadObjv.SetActive(true);
+        }
     }
 }
