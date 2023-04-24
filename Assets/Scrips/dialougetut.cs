a821c60ea18e093528271d043192eca2688354da
Radeah <113103602+Radeah@users.noreply.github.com>
2023-04-24 14:52:59 +0100

    finish cutscene code

--- Assets/Scrips/dialougetut.cs
+++ Assets/Scrips/dialougetut.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class KeypadDoor : MonoBehaviour
+public class dialougetut : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
